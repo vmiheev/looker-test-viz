160878aab27ac16eb0bb311d355157b9c4f525b6
vmiheev <seva.miheev@gmail.com>
2021-11-01 11:39:20 -0400

    trying to change function parameters

--- looker-test-viz.js
+++ looker-test-viz.js
@@ -3,14 +3,14 @@ const visObject = {
 	create: function(element, config){
 		element.innerHTML = "<h1>Ready to render!</h1>";
 	},
-	updateAsync: function(data, element, config, queryResponse, details, doneRendering){
+	updateAsync: function(data, element, config, queryResponse, details, done){
 		var html = "";
 		for(var row of data) {
 			var cell = row[queryResponse.fields.dimensions[0].name];
 			html += LookerCharts.Utils.htmlForCell(cell);
 		}
 		element.innerHTML = html;
-		doneRendering()
+		done()
 	}
 };
 
